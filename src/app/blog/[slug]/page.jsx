6a9ad1865facae9ecdde0e1c7f271ff00cb9b320
Badhon Biswas <biswasbadhon@gmail.com>
2024-01-04 23:22:26 +0600

    Single Blog Post design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,24 @@
+"use client";
+import Link from "next/link";
 import React from "react";
-
+import image from "./image.jpg";
 export default function SlugPage() {
-  return <div>Slug Page</div>;
+  return (
+    <div className="min-h-screen w-full">
+      <section className="text-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+            <img src="./image.jpg" alt="" />
+            <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold dark:text-white">
+              Our Blog
+            </h2>
+            <p className="font-light  sm:text-xl dark:text-gray-400">
+              We use an agile approach to test assumptions and connect with the
+              needs of your audience early and often.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,8 +4,28 @@ import React from "react";
 export default function notfound() {
   return (
     <div className="min-h-screen">
-      <h1>Not Found</h1>
-      <Link href={"/"}>Return Home</Link>
+      <div className="gradient text-white min-h-screen flex items-center">
+        <div className="container mx-auto p-4 flex flex-wrap items-center">
+          <div className="w-full md:w-5/12 text-center p-4"></div>
+          <div className="w-full md:w-7/12 text-center md:text-left p-4">
+            <div className="text-6xl font-medium">404</div>
+            <div className="text-xl md:text-3xl font-medium mb-4">
+              <img
+                className=" w-96 h-96"
+                src="https://www.google.com/search?q=image&oq=image&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIMCAEQABhDGIAEGIoFMgwIAhAAGEMYgAQYigUyDAgDEAAYQxiABBiKBTISCAQQABhDGIMBGLEDGIAEGIoFMhIIBRAAGBQYgwEYhwIYsQMYgAQyEggGEAAYFBiDARiHAhixAxiABDIKCAcQABixAxiABDINCAgQABiDARixAxiABDIHCAkQABiABNIBBzg2N2owajeoAgCwAgA&sourceid=chrome&ie=UTF-8#vhid=Q53WJiavu6IJtM&vssid=l"
+                alt="Not Found"
+              />
+              Oops. This page has gone missing.
+            </div>
+            <div className="text-lg mb-8">
+              You may have mistyped the address or the page may have moved.
+            </div>
+            <Link href="/" className="border border-white rounded p-4">
+              Go Home
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
